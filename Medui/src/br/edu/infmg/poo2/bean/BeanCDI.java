98b891acdb54503a3451d1425319d6330cf199b2
DWalterJansen <davidwalterjansengithub@gmail.com>
2019-01-31 07:44:50 -0200

    Atualização do front-end

---
+++ Medui/src/br/edu/infmg/poo2/bean/BeanCDI.java
@@ -0,0 +1,16 @@
+package br.edu.infmg.poo2.bean;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("beanCDI")
+@SessionScoped
+public class BeanCDI implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	//Colocar @inject e criar metodos necessários
+
+}
